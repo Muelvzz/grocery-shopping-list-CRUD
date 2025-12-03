95da2b7450b81010c195d9732799a52f8d401afa
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-12-03 10:00:46 +0800

    designed the header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [toggleDark, setToggleDark] = useState(false)
+
+  useEffect(() => {
+    if (toggleDark) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [toggleDark])
+
   return (
     <>
+      <header>
+        <div id="header-title">
+          <h1>CRUD Grocery List</h1>
+        </div>
+        <div id="header-btn">
+          <button 
+            id='ui-dark-btn' 
+            onClick={() => setToggleDark(!toggleDark)}
+            style={{backgroundColor: toggleDark ? "white" : "black"}}
+          >
+            {toggleDark ? "🔆" : "🌙"}
+          </button>
+          <button 
+            id='create-btn'
+            style={{color: toggleDark ? "white" : "black"}}
+          >CREATE</button>
+        </div>
+      </header>
     </>
   )
 }
